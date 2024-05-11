f45ac53740feff4dc9bb32d2e1eb83ae10219076
Ogbu Cyprian Omoha <75180026+cysof@users.noreply.github.com>
2024-05-11 19:55:40 +0100

    Update settings.py

--- codebase/setup_file/settings.py
+++ codebase/setup_file/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'cp4reallife@gmail.com'
-EMAIL_HOST_PASSWORD = 'pass64838Cy@'
+EMAIL_HOST_PASSWORD = '@'
 EMAIL_USE_TLS = True  # Set to True if using TLS, False if not
